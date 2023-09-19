72ea4c2c4ab9c745c845500b812c5bf2cb6f201c
Bablukmr <bablukmr800@gmail.com>
2023-09-19 23:09:26 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Banner from './components/Banner/Banner';
 function App() {
   return (
     <div className="App">
-      <h1>Image Slider with Parallax Effect</h1>
+      <div className='header'>
+        <h2>Slider</h2> 
+        </div>
       <Banner/>
     </div>
   );
